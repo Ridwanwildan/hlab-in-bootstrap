ad7716990697ec2b70558206b9c23366c64c083f
Ridwanwildan <rdwnwldn@gmail.com>
2022-02-02 08:14:09 +0700

    add crisp.js

---
+++ js/crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "acaf727c-ea07-40b7-b79b-0c31d45432bd";
+(function() {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
